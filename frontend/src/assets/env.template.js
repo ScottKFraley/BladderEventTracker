c702773e96d622928109b606685b0f4aec2e576a
Scott K. Fraley <scott_fraley@hotmail.com>
2025-08-07 12:24:02 -0700

    Fix Application Insights frontend initialization in production
    - Update env.template.js to use window.appConfig.applicationInsights.connectionString structure
    - Match the path expected by application-insights.service.ts
    - Add env.js to .gitignore to prevent committing local configuration
    - Fixes 'Please provide instrumentation key' error and white page issue
    
    Frontend Application Insights monitoring should now work in both local and production environments.

--- frontend/src/assets/env.template.js
+++ frontend/src/assets/env.template.js
@@ -1,10 +1,11 @@
 // Environment configuration template
 // This file will be processed by nginx to inject runtime environment variables
 (function (window) {
+  window.appConfig = window.appConfig || {};
+  window.appConfig.applicationInsights = {
+    connectionString: '${APPLICATIONINSIGHTS_CONNECTION_STRING}'
+  };
   window.__env = window.__env || {};
-
-  // Environment variables will be injected here by nginx
-  window.__env.APPLICATIONINSIGHTS_CONNECTION_STRING = '${APPLICATIONINSIGHTS_CONNECTION_STRING}';
   window.__env.API_URL = '${API_URL}';
   window.__env.PRODUCTION = '${PRODUCTION}';
 })(this);
